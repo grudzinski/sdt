6e117edcee2d289a53d2dd16666abeb1d0296279
Roman <grudzinski@gmail.com>
2013-07-11 14:14:22 +0300

    Remove duplicate code

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var getStatus = require('./lib/getStatus.js');
 var start = require('./lib/start.js');
+var startMonitored = require('./lib/startMonitored.js');
 var stop = require('./lib/stop.js');
 
 module.exports = {
 	getStatus: getStatus,
+	startMonitored: startMonitored,
 	start: start,
-	stop: stop
+	stop: stop,
 };
