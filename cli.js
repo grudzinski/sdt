0fddb9a1c72d02271125b486020dc6a6baa59684
Roman <grudzinski@gmail.com>
2013-07-11 15:54:41 +0300

    Extract cli to function

--- cli.js
+++ cli.js
@@ -1,65 +1,6 @@
 #!/usr/local/bin/node
 
-var _ = require('underscore');
-var async = require('async');
-
-var getStatus = require('./lib/getStatus.js');
-var start = require('./lib/start.js');
-var startMonitored = require('./lib/startMonitored.js');
-var stop = require('./lib/stop.js');
 var conf = require('./lib/conf.js');
+var runCli = require('./lib/runCli.js');
 
-var args = process.argv;
-
-var commands = {
-	'status': _.partial(getStatus, conf, onStatusGot),
-	'start-monitored': _.partial(startMonitored, conf, onStartMonitoredComplete),
-	'start': _.partial(start, conf, onStartComplete),
-	'stop': _.partial(stop, conf, onStopComplete)
-};
-
-if (args.length > 2) {
-	var commandName = args[2];
-	var command = commands[commandName];
-	if (command) {
-		command(conf);
-	}
-} else {
-	console.log('Commands: status, start, stop');
-}
-
-function handleError(message, err) {
-	if (err) {
-		console.log(message, err);
-		return true;
-	}
-	return false;
-}
-
-function onStartMonitoredComplete(err, pid) {
-	if (handleError('Fail to start monitored daemon: %s', err)) {
-		return;
-	}
-	console.log('Monitored daemon started with pid: ' + pid);
-}
-
-function onStartComplete(err, pid) {
-	if (handleError('Fail to start daemon: %s', err)) {
-		return;
-	}
-	console.log('Daemon started with pid: ' + pid);
-}
-
-function onStopComplete(err, alreadyStopped) {
-	if (handleError('Fail to stop: %s', err)) {
-		return;
-	}
-	console.log('Daemon was ' + (alreadyStopped ? 'already stopped' : 'stopped'));
-}
-
-function onStatusGot(err, status) {
-	if (handleError('Fail to get status: %s', err)) {
-		return;
-	}
-	console.log('Daemon %s', (status ? 'is running' : 'was stopped'));
-}
+runCli(conf);
--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var getStatus = require('./lib/getStatus.js');
 var start = require('./lib/start.js');
 var startMonitored = require('./lib/startMonitored.js');
 var stop = require('./lib/stop.js');
+var runCli = require('./lib/runCli.js');
 
 module.exports = {
 	getStatus: getStatus,
 	startMonitored: startMonitored,
 	start: start,
 	stop: stop,
+	runCli: runCli
 };
